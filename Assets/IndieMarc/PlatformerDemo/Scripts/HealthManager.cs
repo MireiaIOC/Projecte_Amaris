628152ebde3fa5699c539f497e1e9470fa6785c5
MireiaIOC <81866222+MireiaIOC@users.noreply.github.com>
2022-10-07 13:38:10 +0200

    Prototype

---
+++ Assets/IndieMarc/PlatformerDemo/Scripts/HealthManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    HealthPlayer healthPlayer;
+    public int amount;
+   
+    
+    
+    void Start()
+    {
+        healthPlayer = GameObject.FindWithTag("Player").GetComponent<HealthPlayer>();
+    }
+
+   public void AddHealth(int amount)
+    {
+        healthPlayer.health += amount;
+        if (healthPlayer.health > 100) { healthPlayer.health = 100; }
+
+
+    }
+
+    public void RestHealth(int amount)
+    {
+        healthPlayer.health -= amount;
+        if (healthPlayer.health < 0) { healthPlayer.health = 0; }
+
+
+    }
+
+}
